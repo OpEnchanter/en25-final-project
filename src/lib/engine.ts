fb14110f9321d4bc4a05106ccab35983d56730bf
Tristan Greenhagen <openchanter@gmail.com>
2026-05-24 15:04:26 -0400

    feat: working rigidbodies

--- src/lib/engine.ts
+++ src/lib/engine.ts
@@ -1,6 +1,51 @@
+import chalk from "chalk"
+
 export type vector = {
     x: number,
     y: number
+}
+
+export type CollisionData = {
+    collisionVector: vector,
+    collisionObjectPosition: vector,
+    collisionNormal: vector,
+    collisionObjectBounds: vector,
+    object: null | GameObject
+}
+
+export type BodyProperties = {
+    bounciness: number,
+    density: number,
+    drag: number,
+    friction: number
+}
+
+export class vMath {
+    public static dot(a: vector, b: vector): number {
+        return (a.x*b.x) + (a.y*b.y);
+    }
+
+    public static magnitude(a: vector): number {
+        return Math.sqrt(a.x**2 + a.y**2)
+    }
+
+    public static normalize(a: vector): vector {
+        const m = this.magnitude(a);
+        if (m===0) return {x:0, y:0}
+        return {x:a.x / m, y:a.y / m} as vector
+    }
+
+    public static multiply(a: vector, b: number) {
+        return {x:a.x*b, y:a.y*b} as vector
+    }
+
+    public static subtract(a: vector, b: vector) {
+        return {x:a.x-b.x, y:a.y-b.y}
+    }
+
+    public static add(a: vector, b: vector) {
+        return {x:a.x+b.x, y:a.y+b.y}
+    }
 }
 
 export function fixScale(canvas: HTMLCanvasElement, downscaleFactor: number) {
@@ -27,13 +72,16 @@ class ComponentBase {
         this.object = object ? object : null;
     }
 
+    onCollisionUpdate(): void {};
+    onPhysicsUpdate(data: Array<CollisionData>): void {};
+
     onUpdate(): void {};
-    onCollisionEnter(): void {};
+    onCollisionEnter(params: CollisionData): void {};
     onCollisionExit(): void {};
-    onCollisionStay(): void {};
-    onTriggerEnter(): void {};
+    onCollisionStay(params: CollisionData): void {};
+    onTriggerEnter(params: CollisionData): void {};
     onTriggerExit(): void {};
-    onTriggerStay(): void {};
+    onTriggerStay(params: CollisionData): void {};
 
     onLateUpdate(): void {};
 
@@ -81,7 +129,12 @@ export class Renderer extends ComponentBase {
     }
 
     override onLateUpdate(): void {
-        draw(this.ctx, this.sprite?.texture as HTMLImageElement, this.transform?.rotation as number, this.transform?.position as vector, this.transform?.scale as vector)
+        if (!this.transform) {return}
+        const p = {
+            x: Math.round(this.transform.position.x),
+            y: Math.round(this.transform.position.y)
+        } as vector
+        draw(this.ctx, this.sprite?.texture as HTMLImageElement, this.transform?.rotation as number, p, this.transform?.scale as vector)
     }
 }
 
@@ -108,7 +161,7 @@ export class BoxCollider extends ComponentBase {
         });
     }
 
-    override onUpdate(): void {
+    override onCollisionUpdate(): void {
         if (!this.transform || !this.object) { return }
         this.object.isColliding = false;
 
@@ -118,30 +171,51 @@ export class BoxCollider extends ComponentBase {
         for (let i = 0; i < this.colliders.length; i++) {
             const b = this.colliders[i]?.bounds;
             const p = this.transforms[i]?.position;
-            if (!b || !p) { return }
+            if (!b || !p) { continue }
 
-            if (tp.x + tb.x / 2 > p.x - b.x / 2) {
-                this.object.isColliding = true
-            }
+            const ax = tp.x
+            const ay = tp.y
 
-            if (tp.x - tb.x / 2 > p.x + b.x / 2) {
-                this.object.isColliding = true
-            }
+            const bx = p.x
+            const by = p.y
 
-            if (tp.y + tb.y / 2 > p.y - b.y / 2) {
-                this.object.isColliding = true
-            }
+            const dx = ax - bx
+            const dy = ay - by
 
-            if (tp.y - tb.y / 2 > p.y + b.y / 2) {
-                this.object.isColliding = true
+            const px = (tb.x / 2 + b.x / 2) - Math.abs(dx)
+            const py = (tb.y / 2 + b.y / 2) - Math.abs(dy)
+
+            if (px > 0 && py > 0) {
+                this.object.isColliding = true;
+
+                let pv: vector = {
+                    x: dx<0 ? px : -px, 
+                    y: dy<0 ? py : -py
+                };
+
+                let mtv: vector;
+                let nv: vector;
+
+                if (px < py) {
+                    mtv = { x: pv.x, y: 0 };
+                    nv  = { x: dx < 0 ? -1 : -1, y: 0 };
+                } else {
+                    mtv = { x: 0, y: pv.y };
+                    nv  = { x: 0, y: dy < 0 ? -1 : -1 };
+                }
+
+                this.object.collisionData.push({
+                    collisionVector: mtv,
+                    collisionObjectPosition: {
+                        x: p.x,
+                        y: p.y
+                    },
+                    collisionNormal: nv,
+                    collisionObjectBounds: b,
+                    object: this.colliders[i]?.object as GameObject
+                })
             }
         }
-
-        if (tp.y + tb.y / 2 > this.object.app.canvas.height) {
-            this.object.isColliding = true
-        }
-
-        console.log(this.object.isColliding);
     }
 }
 
@@ -149,40 +223,103 @@ export class Rigidbody extends ComponentBase {
     velocity: vector = {x:0, y:0};
     transform: Transform | null = null;
     lastPos: vector | null = null;
+    public bodyProps;
+
+    public lastCollisionReflection: vector = {x:0, y:0};
+    public lastCollisionPosition: vector = {x:0, y:0};
+
+    boxCollider: BoxCollider | null = null;
+
+    constructor(bodyProps: BodyProperties) {
+        super();
+        this.bodyProps = bodyProps;
+    }
 
     override onInitialized(): void {
         this.transform = this.object?.getComponents(Transform)[0] as Transform;
+        this.boxCollider = this.object?.getComponents(BoxCollider)[0] as BoxCollider;
         this.lastPos = this.transform.position;
     }
 
-    override onUpdate(): void {
+    override onPhysicsUpdate(data: Array<CollisionData>): void {
+        let anyFloorCol: boolean = false;
+        for (const c of data) {
+            if (c.collisionNormal.y < 0) {
+                anyFloorCol = true;
+            }
+        }
+        if (anyFloorCol === false) {
+            this.velocity = {x:this.velocity.x, y:(this.velocity.y+0.1)};
+        }
+
+        // Interaction
+        for (const params of data) {
+
+            let b = {
+                x: -params.collisionNormal.y,
+                y: params.collisionNormal.x
+            }
+            
+            if (this.transform) {
+                this.transform.position = {
+                    x: this.transform.position.x - params.collisionVector.x,
+                    y: this.transform.position.y - params.collisionVector.y,
+                }
+            }
+
+            if (this.object?.isColliding && !this.object.isCollidingOld) {
+                const a = vMath.normalize(this.velocity);
+                
+                const scalar = (vMath.dot(a, b) / vMath.magnitude(b)**2) * 2
+                const scaledB = vMath.multiply(b, scalar)
+
+                const r = vMath.subtract(scaledB, a);
+
+                this.lastCollisionReflection = r;
+                this.lastCollisionPosition = this.transform ? this.transform?.position : {x:0, y:0};
+
+                const otherBody = params.object;
+                if (!otherBody) continue
+                const obRb = otherBody.getComponents(Rigidbody)[0] as Rigidbody
+                if (obRb) {
+                    const obDesnity = obRb.bodyProps.density;
+
+                    const density = this.bodyProps.density;
+
+                    const totalDensity = obDesnity + density;
+
+                    obRb.velocity = {
+                        x: obRb.velocity.x + vMath.normalize(this.velocity).x * (vMath.magnitude(this.velocity) * (obDesnity/totalDensity)),
+                        y: obRb.velocity.y + vMath.normalize(this.velocity).y * (vMath.magnitude(this.velocity) * (obDesnity/totalDensity))
+                    }
+
+                    this.velocity = {
+                        x:r.x * (vMath.magnitude(this.velocity) * (density/totalDensity)), 
+                        y:r.y * (vMath.magnitude(this.velocity) * (density/totalDensity))
+                    }
+                } else {
+                    this.velocity = {
+                        x:r.x * vMath.magnitude(this.velocity) * (-1*((1-this.bodyProps.bounciness)*-b.y) + 1) * (-1*((1-this.bodyProps.friction)*b.x) + 1), 
+                        y:r.y * vMath.magnitude(this.velocity) * (-1*((1-this.bodyProps.bounciness)*b.x) + 1) * (-1*((1-this.bodyProps.friction)*b.y) + 1)
+                    }
+                }
+            } else if (this.object?.isColliding && this.object.isCollidingOld) {
+                this.velocity.x *= (-1*((1-this.bodyProps.friction)*b.x) + 1)
+                this.velocity.y *= (-1*((1-this.bodyProps.friction)*b.y) + 1)
+
+                this.velocity.x -= params.collisionVector.x;
+                this.velocity.y -= params.collisionVector.y;
+            }
+        }
+
+        this.velocity = {x:this.velocity.x*this.bodyProps.drag, y:this.velocity.y*this.bodyProps.drag};
+
         if (this.transform) {
             this.transform.position = {
                 x:this.transform?.position.x as number + this.velocity.x,
                 y:this.transform?.position.y as number + this.velocity.y
-            }
+            };
         }
-        if (!this.object?.isColliding) {
-            this.velocity = {x:this.velocity.x, y:(this.velocity.y+0.2)}
-        }
-    }
-
-    override onCollisionStay(): void {
-        if (this.transform && this.object) {
-            this.transform.position = {
-                x:this.transform?.position.x as number - this.velocity.x,
-                y:this.object?.app.canvas.height - 7
-            }
-        }
-        this.velocity = {
-            x:0, 
-            y:0
-        }
-        console.log("COLLISION");
-    }
-
-    override onLateUpdate(): void {
-        this.lastPos = this.transform?.position ? this.transform.position : null;
     }
 }
 
@@ -207,9 +344,6 @@ export class PlayerController extends ComponentBase {
                 this.transform.position = {x: this.transform?.position.x, y: (this.transform?.position.y as number)-1}
                 this.rigidbody.velocity = {x: this.rigidbody?.velocity.x, y: this.rigidbody.velocity.y-0.5}
             }
-            if (this.keys["s"]) {
-                this.transform.position = {x: this.transform?.position.x, y: (this.transform?.position.y as number)+1}
-            }
 
             if (this.keys["a"]) {
                 this.rigidbody.velocity = {x: this.rigidbody?.velocity.x-0.1, y: this.rigidbody.velocity.y}
@@ -226,8 +360,10 @@ export class GameObject {
     public isColliding: boolean = false;
     public isTriggerred: boolean = false;
 
-    private isCollidingOld: boolean = false;
-    private isTriggerredOld: boolean = false;
+    public collisionData: Array<CollisionData> = [];
+
+    public isCollidingOld: boolean = false;
+    public isTriggerredOld: boolean = false;
 
     public app: App;
 
@@ -253,13 +389,19 @@ export class GameObject {
 
     public onUpdate() {
 
+        this.collisionData = [];
+
         for (const m of this.Components) {
+            m.onCollisionUpdate();
+            m.onPhysicsUpdate(this.collisionData);
             m.onUpdate();
         }
 
         if (this.isColliding && !this.isCollidingOld) {
             for (const m of this.Components) {
-                m.onCollisionEnter();
+                for (const col of this.collisionData) {
+                    m.onCollisionEnter(col);
+                }
             }
         } else if (!this.isColliding && this.isCollidingOld) {
             for (const m of this.Components) {
@@ -269,19 +411,25 @@ export class GameObject {
 
         if (this.isColliding && this.isCollidingOld) {
             for (const m of this.Components) {
-                m.onCollisionStay();
+                for (const col of this.collisionData) {
+                    m.onCollisionStay(col);
+                }
             }
         }
         
         if (this.isTriggerred && this.isTriggerredOld) {
             for (const m of this.Components) {
-                m.onTriggerStay();
+                for (const col of this.collisionData) {
+                    m.onTriggerStay(col);
+                }
             }
         }
 
         if (this.isTriggerred && !this.isTriggerredOld) {
             for (const m of this.Components) {
-                m.onTriggerEnter();
+                for (const col of this.collisionData) {
+                    m.onTriggerEnter(col);
+                }
             }
         } else if (!this.isTriggerred && this.isTriggerredOld) {
             for (const m of this.Components) {
@@ -338,14 +486,15 @@ export class App {
             return canvas;
         })());
         this.ctx = this.canvas.getContext("2d");
+        this.ctx.imageSmoothingEnabled = false
     }
 
     addObject(obj: GameObject) {
         this.objects.push(obj);
     }
 
-    start() {
-        console.log("[Info] App starting!")
+    start(targetFramerate: number) {
+        console.log(`[${chalk.blueBright("Info")}] App starting!`)
         document.addEventListener("DOMContentLoaded", () => {
             let t = 0;
             this.objects.forEach(object => {
@@ -359,7 +508,7 @@ export class App {
                     object.onUpdate();
                 }
                 t++;
-            }, 1000/60)
+            }, 1000/targetFramerate)
         })
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,77 @@
 import * as Engine from "./lib/engine.ts";
+import chalk from "chalk";
 const app = new Engine.App({
     downscaleFactor: 4
 });
 
+console.log(chalk.black(`
+┌-------------------------------------------------------┐
+|                    ${chalk.bold("Super Mr. Bennet")}                   |
+|           ${chalk.italic("By Nathan Le and Tristan Greenhagen")}         |
+└-------------------------------------------------------┘
+`));
+
+console.log(chalk.italic("Based on works by Nintendo and Jane Austen"))
+
+for (let b = 0; b < 0; b++) {
+    for (let i = 0; i < 6; i++) {
+        let o: Engine.GameObject = new Engine.GameObjectBuilder(app)
+            .addComponent(new Engine.Transform({x:64+(16*b), y:64+(16*i)}, 0, {x:16, y:16}))
+            .addComponent(new Engine.Sprite("/src/assets/smorf.png"))
+            .addComponent(new Engine.Renderer(app.ctx))
+            .addComponent(new Engine.BoxCollider({x: 16, y: 16}))
+            .addComponent(new Engine.Rigidbody({
+                bounciness: 0,
+                friction: 0,
+                drag: 0.9,
+                density: 1
+            }))
+            .build();
+        (o.getComponents(Engine.Rigidbody)[0] as Engine.Rigidbody).velocity = {x:4, y:4}
+        app.addObject(o)
+    }
+}
+
 app.addObject(new Engine.GameObjectBuilder(app)
-    .addComponent(new Engine.Transform({x:12, y:12}, 0, {x:24, y:24}))
-    .addComponent(new Engine.Sprite("/src/assets/test.png"))
+    .addComponent(new Engine.Sprite("/src/assets/smorf.png"))
     .addComponent(new Engine.Renderer(app.ctx))
+    .addComponent(new Engine.Transform({x:64, y:64}, 0, {x:32, y:32}))
+    .addComponent(new Engine.BoxCollider({x: 32, y: 32}))
+    .addComponent(new Engine.Rigidbody({
+        bounciness: 0,
+        friction: 0.98,
+        drag: 0.98,
+        density: 1
+    }))
     .addComponent(new Engine.PlayerController())
-    .addComponent(new Engine.BoxCollider({x: 16, y: 16}))
-    .addComponent(new Engine.Rigidbody())
     .build())
 
-app.start();
+app.addObject(new Engine.GameObjectBuilder(app)
+    .addComponent(new Engine.Sprite("/src/assets/image.png"))
+    .addComponent(new Engine.Renderer(app.ctx))
+    .addComponent(new Engine.Transform({x:256, y:256}, 0, {x:512, y:96}))
+    .addComponent(new Engine.BoxCollider({x: 512, y: 96}))
+    .build())
+
+app.addObject(new Engine.GameObjectBuilder(app)
+    .addComponent(new Engine.Sprite("/src/assets/image.png"))
+    .addComponent(new Engine.Renderer(app.ctx))
+    .addComponent(new Engine.Transform({x:0, y:256}, 0, {x:96, y:512}))
+    .addComponent(new Engine.BoxCollider({x: 96, y: 512}))
+    .build())
+
+app.addObject(new Engine.GameObjectBuilder(app)
+    .addComponent(new Engine.Sprite("/src/assets/image.png"))
+    .addComponent(new Engine.Renderer(app.ctx))
+    .addComponent(new Engine.Transform({x:256, y:256}, 0, {x:96, y:512}))
+    .addComponent(new Engine.BoxCollider({x: 96, y: 512}))
+    .build())
+
+app.addObject(new Engine.GameObjectBuilder(app)
+    .addComponent(new Engine.Sprite("/src/assets/image.png"))
+    .addComponent(new Engine.Renderer(app.ctx))
+    .addComponent(new Engine.Transform({x:256, y:0}, 0, {x:512, y:96}))
+    .addComponent(new Engine.BoxCollider({x: 512, y: 96}))
+    .build())
+
+app.start(60);
