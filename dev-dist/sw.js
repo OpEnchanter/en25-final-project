aeed35d0731f431e31d8bf723eb53023f11f2599
Tristan Greenhagen <openchanter@gmail.com>
2026-06-17 18:24:07 -0400

    fix: fixed final level being impossible and fixed innapropriate dialogue

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-c3369df1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f562a2j15j8"
+    "revision": "0.okdqt6899k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
